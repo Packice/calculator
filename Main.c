5d27a0405ce0a0c195bfae49ceda4f02449ec31e
Packed ice <112376986+Packice@users.noreply.github.com>
2023-10-01 11:16:06 +0800

    Add files via upload

---
+++ Main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    for(int a ; a = 1000000000 ; )
+    {
+    printf("第一个数字\n");    
+    int two;
+    int one;
+    //定义变量
+    scanf("%d",&one);
+    printf("and");
+    scanf("%d",&two);
+    a = two + one;
+    printf("%d\n",a);
+    //计算并输出
+    }
+    return 0;
+}
